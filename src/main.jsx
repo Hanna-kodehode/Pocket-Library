bffcc8e0e9069be5851e2a5d2c868d04dedf537e
Hanna <hanna.kodehode@gmail.com>
2025-02-10 15:17:44 +0100

    change links

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: "/Pocket-Library/Home",
         element: <App />,
       },
       {
