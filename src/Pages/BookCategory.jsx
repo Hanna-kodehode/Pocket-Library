c5111f1d36fc31af7635150b69cf872d7d744c83
Hanna <hanna.kodehode@gmail.com>
2025-02-10 13:46:55 +0100

    kms

--- src/Pages/BookCategory.jsx
+++ src/Pages/BookCategory.jsx
@@ -1,11 +1,40 @@
 import { useState, useEffect } from "react";
+
 import { useParams } from "react-router-dom";
+
 import BookCard from "../components/BookCard.jsx";
 
 export default function BookCategory() {
+  const { category } = useParams();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setBooks([]);
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(
+          `https://gutendex.com/books?topic=${category.toLowerCase()}`
+        );
+        const data = await response.json();
+        console.log(data.results);
+        setBooks([...data.results]);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    };
+
+    fetchBooks();
+  }, [category]);
+
   return (
-    <div className="bookCards">
-      <BookCard key={book.id} book={book} />
-    </div>
+    <>
+      <div className="bookCards">
+        <p>{books.length == 0 ? "Loading..." : ""}</p>
+        {books &&
+          books.map((book) => {
+            return <BookCard key={book.id} bookData={book} />;
+          })}
+      </div>
+    </>
   );
 }
